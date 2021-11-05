06cf1a8c4a0307e349f3e3220344866041cdd414
Chiedu <iamnedu912@gmail.com>
2021-11-05 15:34:28 +0100

    Added route to pay-in-4-easy-payments

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,19 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+const FourEasyPayments = () =>
+    import(
+        /* webpackChunkName: 'FourEasyPayments' */ '../forms/FourEasyPayments'
+    );
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+    {
+        path: '/pay-in-4-easy-payments',
+        name: 'foureasypayments',
+        component: FourEasyPayments,
+    },
+];
 
 const router = new VueRouter({
     mode: 'history',
