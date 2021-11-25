96246d130270704d11fa26abd26970396b8a9c7f
Chiedu <iamnedu912@gmail.com>
2021-11-25 16:24:57 +0100

    Added passport to payload

--- src/mixins/gotoNextModal.js
+++ src/mixins/gotoNextModal.js
@@ -1,50 +1,49 @@
 const gotoNextModalMixin = {
-	data() {
-		return {
-			tel: '',
-			payload: {
-				dialCode: '',
-				password: '',
-				otp: '',
-				active: false,
-				bvn: '',
-				email: '',
-			},
-			form: {
-				terms: false,
-			},
-			loginPayload: {
-				terms: false,
-				password: '',
-				email: '',
-			},
-		};
-	},
-	watch: {
-		payload: {
-			deep: true,
-			handler(value) {
-				this.tel = `+${value.dialCode} `;
-			},
-		},
-	},
-	methods: {
-		onSelect({ dialCode: dialCode }) {
-			this.payload.dialCode = dialCode;
-		},
-		saveOtpCode(code) {
-			this.payload.otp = code;
-		},
-		gotoNextModal(isValid, data, nextModal) {
-			if (!isValid) {
-				this.$emit('gotoNextModal', {
-					next: nextModal,
-					payload: { ...data },
-				});
-			}
-		}
-	}
+    data() {
+        return {
+            tel: '',
+            payload: {
+                dialCode: '',
+                password: '',
+                otp: '',
+                active: false,
+                bvn: '',
+                email: '',
+                passport: null,
+            },
+            form: {
+                terms: false,
+            },
+            loginPayload: {
+                terms: false,
+                password: '',
+                email: '',
+            },
+        };
+    },
+    watch: {
+        payload: {
+            deep: true,
+            handler(value) {
+                this.tel = `+${value.dialCode} `;
+            },
+        },
+    },
+    methods: {
+        onSelect({ dialCode: dialCode }) {
+            this.payload.dialCode = dialCode;
+        },
+        saveOtpCode(code) {
+            this.payload.otp = code;
+        },
+        gotoNextModal(isValid, data, nextModal) {
+            if (!isValid) {
+                this.$emit('gotoNextModal', {
+                    next: nextModal,
+                    payload: { ...data },
+                });
+            }
+        },
+    },
 };
 export default gotoNextModalMixin;
-
-
