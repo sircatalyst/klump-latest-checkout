e68935cdb9db381cb60b232fc9394d1cd9cbc539
Chiedu <iamnedu912@gmail.com>
2022-01-04 22:23:05 +0100

    Added Sign in feature for returning users

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,63 @@
 <template>
-	<div>
-		<four-easy-payments v-show="showFourEasyPaymentsModal" @gotoNextModal="handleGotoNextModal" />
-		<verify-your-details v-show="showVerifyYourDetailsModal" @gotoNextModal="handleGotoNextModal" />
-		<enter-the-code v-show="showEnterTheCodeModal" @gotoNextModal="handleGotoNextModal" />
-		<whats-your-email v-show="showWhatsYourEmailModal" @gotoNextModal="handleGotoNextModal" />
-		<choose-a-password v-show="showChooseAPasswordModal" @gotoNextModal="handleGotoNextModal" />
-		<complete-your-account v-show="showCompleteYourAccountModal" @gotoNextModal="handleGotoNextModal" />
-		<choose-identification v-show="showChooseIdentificationModal" @gotoNextModal="handleGotoNextModal" />
-		<passport-verification v-show="showPassportVerificationModal" @gotoNextModal="handleGotoNextModal" />
-		<whats-your-bvn v-show="showWhatsYourBvnModal" @gotoNextModal="handleGotoNextModal" />
-		<connect-your-bank v-show="showConnectYourBankModal" @gotoNextModal="handleGotoNextModal" />
-		<complete-checkout v-show="showCompleteCheckoutModal" @gotoNextModal="handleGotoNextModal" />
-		<four-interest-free-installment v-show="showFourInterestFreeInstallmentModal" @gotoNextModal="handleGotoNextModal" />
-		<successful-transaction v-show="showSuccessfulTransactionModal" @gotoNextModal="handleGotoNextModal" />
-	</div>
+    <div>
+        <four-easy-payments
+            v-show="showFourEasyPaymentsModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <verify-your-details
+            v-show="showVerifyYourDetailsModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <enter-the-code
+            v-show="showEnterTheCodeModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <whats-your-email
+            v-show="showWhatsYourEmailModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <choose-a-password
+            v-show="showChooseAPasswordModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <complete-your-account
+            v-show="showCompleteYourAccountModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <choose-identification
+            v-show="showChooseIdentificationModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <passport-verification
+            v-show="showPassportVerificationModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <whats-your-bvn
+            v-show="showWhatsYourBvnModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <connect-your-bank
+            v-show="showConnectYourBankModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <complete-checkout
+            v-show="showCompleteCheckoutModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <four-interest-free-installment
+            v-show="showFourInterestFreeInstallmentModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <successful-transaction
+            v-show="showSuccessfulTransactionModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <sign-in
+            v-show="showSignInModal"
+            @gotoNextModal="handleGotoNextModal"
+        />
+        <pay v-show="showPayModal" @gotoNextModal="handleGotoNextModal" />
+    </div>
 </template>
 
 <script>
@@ -30,108 +74,122 @@ import ConnectYourBank from '@/forms/profiling/ConnectYourBank';
 import CompleteCheckout from '@/forms/profiling/CompleteCheckout';
 import FourInterestFreeInstallment from '@/forms/checkout/FourInterestFreeInstallment';
 import SuccessfulTransaction from '@/forms/returning_user/SuccessfulTransaction';
+import SignIn from '@/forms/returning_user/SignIn';
+import Pay from '@/forms/returning_user/Pay';
 
 export default {
     name: 'Home',
-	components: {
-		FourEasyPayments,
-		VerifyYourDetails,
-		EnterTheCode,
-		WhatsYourEmail,
-		ChooseAPassword,
-		CompleteYourAccount,
-		ChooseIdentification,		
-		PassportVerification,
-		WhatsYourBvn,
-		ConnectYourBank,
-		CompleteCheckout,
-		FourInterestFreeInstallment,
-		SuccessfulTransaction
-	},
-	data() {
-		return {
-			showFourEasyPaymentsModal: true,
-			showVerifyYourDetailsModal: false,
-			showEnterTheCodeModal: false,
-			showWhatsYourEmailModal: false,
-			showChooseAPasswordModal: false,
-			showCompleteYourAccountModal: false,
-			showWhatsYourBvnModal: false,
-			showConnectYourBankModal: false,
-			showCompleteCheckoutModal: false,
-			showChooseIdentificationModal: false,
-			showPassportVerificationModal: false,
-			showFourInterestFreeInstallmentModal: false,
-			showSuccessfulTransactionModal: false,
-			payload: {}
-		}
-	},
-	methods: {
-		handleGotoNextModal(param) {
-			switch (param.next) {
+    components: {
+        FourEasyPayments,
+        VerifyYourDetails,
+        EnterTheCode,
+        WhatsYourEmail,
+        ChooseAPassword,
+        CompleteYourAccount,
+        ChooseIdentification,
+        PassportVerification,
+        WhatsYourBvn,
+        ConnectYourBank,
+        CompleteCheckout,
+        FourInterestFreeInstallment,
+        SuccessfulTransaction,
+        SignIn,
+        Pay,
+    },
+    data() {
+        return {
+            showFourEasyPaymentsModal: true,
+            showVerifyYourDetailsModal: false,
+            showEnterTheCodeModal: false,
+            showWhatsYourEmailModal: false,
+            showChooseAPasswordModal: false,
+            showCompleteYourAccountModal: false,
+            showWhatsYourBvnModal: false,
+            showConnectYourBankModal: false,
+            showCompleteCheckoutModal: false,
+            showChooseIdentificationModal: false,
+            showPassportVerificationModal: false,
+            showFourInterestFreeInstallmentModal: false,
+            showSuccessfulTransactionModal: false,
+            showSignInModal: false,
+            showPayModal: false,
+            payload: {},
+        };
+    },
+    methods: {
+        handleGotoNextModal(param) {
+            switch (param.next) {
                 case 'verifyYourDetailsModal':
-					this.showFourEasyPaymentsModal = false;
-					this.showVerifyYourDetailsModal = true;
+                    this.showFourEasyPaymentsModal = false;
+                    this.showVerifyYourDetailsModal = true;
+                    break;
+                case 'signInModal':
+                    this.showFourEasyPaymentsModal = false;
+                    this.showSignInModal = true;
+                    break;
+                case 'payModal':
+                    this.showSignInModal = false;
+                    this.showPayModal = true;
                     break;
                 case 'enterTheCodeModal':
-					this.payload = param.payload;
-					this.showVerifyYourDetailsModal = false;
-					this.showEnterTheCodeModal = true;
+                    this.payload = param.payload;
+                    this.showVerifyYourDetailsModal = false;
+                    this.showEnterTheCodeModal = true;
                     break;
                 case 'whatsYourEmailModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showEnterTheCodeModal = false;
-					this.showWhatsYourEmailModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showEnterTheCodeModal = false;
+                    this.showWhatsYourEmailModal = true;
                     break;
                 case 'chooseAPasswordModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showWhatsYourEmailModal = false;
-					this.showChooseAPasswordModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showWhatsYourEmailModal = false;
+                    this.showChooseAPasswordModal = true;
                     break;
                 case 'completeYourAccountModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showChooseAPasswordModal = false;
-					this.showCompleteYourAccountModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showChooseAPasswordModal = false;
+                    this.showCompleteYourAccountModal = true;
                     break;
                 case 'chooseIdentificationModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showCompleteYourAccountModal = false;
-					this.showChooseIdentificationModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showCompleteYourAccountModal = false;
+                    this.showChooseIdentificationModal = true;
                     break;
                 case 'passportVerificationModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showChooseIdentificationModal = false;
-					this.showPassportVerificationModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showChooseIdentificationModal = false;
+                    this.showPassportVerificationModal = true;
                     break;
                 case 'whatsYourBvnModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showPassportVerificationModal = false;
-					this.showWhatsYourBvnModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showPassportVerificationModal = false;
+                    this.showWhatsYourBvnModal = true;
                     break;
                 case 'connectYourBankModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showWhatsYourBvnModal = false;
-					this.showConnectYourBankModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showWhatsYourBvnModal = false;
+                    this.showConnectYourBankModal = true;
                     break;
                 case 'completeCheckoutModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showConnectYourBankModal = false;
-					this.showCompleteCheckoutModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showConnectYourBankModal = false;
+                    this.showCompleteCheckoutModal = true;
                     break;
                 case 'fourInterestFreeInstallmentModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showCompleteCheckoutModal = false;
-					this.showFourInterestFreeInstallmentModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showCompleteCheckoutModal = false;
+                    this.showFourInterestFreeInstallmentModal = true;
                     break;
                 case 'successfulTransactionModal':
-					this.payload = { ...param.payload, ...this.payload };
-					this.showFourInterestFreeInstallmentModal = false;
-					this.showSuccessfulTransactionModal = true;
+                    this.payload = { ...param.payload, ...this.payload };
+                    this.showFourInterestFreeInstallmentModal = false;
+                    this.showSuccessfulTransactionModal = true;
                     break;
-				default:
-				this.showFourEasyPaymentsModal = true;
+                default:
+                    this.showFourEasyPaymentsModal = true;
             }
-		}
-	}
+        },
+    },
 };
 </script>
